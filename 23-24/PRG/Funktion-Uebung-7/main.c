c3921d81649faf3ec2ff972f453f3af7a9f14d9e
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-03-19 19:55:52 +0100

    feat(Funktion-Uebung-7): solution for task 1

--- 23-24/PRG/Funktion-Uebung-7/main.c
+++ 23-24/PRG/Funktion-Uebung-7/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+int stringIndex(char text[], char symbol);
+
+int stringIndex(char text[], char symbol){
+    int length = sizeof(text);
+    int position = -1;
+
+    for(int i = 0; i < length; i++){
+        if(text[i] == symbol){
+            position = i;
+            i = length;
+        }
+    }
+
+    return position;
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    printf("pos: %d\n\n", stringIndex("test",'e'));
     return 0;
 }
