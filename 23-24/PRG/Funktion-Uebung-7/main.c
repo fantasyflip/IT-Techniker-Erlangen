76faa43a6484c3df1e8fb174f8f0df3bc21aab59
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-03-19 20:26:46 +0100

    feat(Funktion-Uebung-7): solution for task 2

--- 23-24/PRG/Funktion-Uebung-7/main.c
+++ 23-24/PRG/Funktion-Uebung-7/main.c
@@ -2,12 +2,11 @@
 #include <string.h>
 
 int stringIndex(char text[], char symbol);
+int enthaeltString(char wrappingText[], char innerText[]);
 
 int stringIndex(char text[], char symbol){
     int length = strlen(text);
     int position = -1;
-
-    printf("%d",length);
 
     for(int i = 0; i < length; i++){
         if(text[i] == symbol){
@@ -19,8 +18,34 @@ int stringIndex(char text[], char symbol){
     return position;
 }
 
+int enthaeltString(char wrappingText[], char innerText[]){
+    int isContaining = 0;
+    int wrappingTextLength = strlen(wrappingText);
+    int innerTextLength = strlen(innerText);
+
+    if(wrappingTextLength < innerTextLength) return 0;
+
+    for(int i = 0; i < wrappingTextLength; i++){
+        int matchingChars = 0;
+        for(int j = 0; j < innerTextLength; j++){
+            if(wrappingText[i+j] == innerText[j]){
+                matchingChars++;
+                if(matchingChars == innerTextLength){
+                    isContaining = 1;
+                    i = wrappingTextLength;
+                    j = innerTextLength;
+                }
+            } else {
+                j = innerTextLength;
+            }
+        }
+    }
+
+    return isContaining;
+}
+
 int main()
 {
-    printf("pos: %d\n\n", stringIndex("test",'p'));
+    printf("enthaelt: %d\n\n", enthaeltString("abctestefg","test"));
     return 0;
 }
