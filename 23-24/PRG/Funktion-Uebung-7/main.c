f75035db257c99423b3b9bf0446b8ac576e74455
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-03-19 20:01:28 +0100

    fix(Funktion-Uebung-7): get proper length of char array

--- 23-24/PRG/Funktion-Uebung-7/main.c
+++ 23-24/PRG/Funktion-Uebung-7/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int stringIndex(char text[], char symbol);
 
 int stringIndex(char text[], char symbol){
-    int length = sizeof(text);
+    int length = strlen(text);
     int position = -1;
+
+    printf("%d",length);
 
     for(int i = 0; i < length; i++){
         if(text[i] == symbol){
@@ -18,6 +21,6 @@ int stringIndex(char text[], char symbol){
 
 int main()
 {
-    printf("pos: %d\n\n", stringIndex("test",'e'));
+    printf("pos: %d\n\n", stringIndex("test",'p'));
     return 0;
 }
