61328ee24afbfb9f63de52b22d280149724fcb81
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-05-07 20:12:37 +0200

    feat(Pointer-Uebung-8): solution for task 1

--- 23-24/PRG/Pointer-Uebung-8/main.c
+++ 23-24/PRG/Pointer-Uebung-8/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+void task1(int* a, int* b);
+
+void task1(int* a, int* b){
+    int temp;
+
+    temp = *a;  // Wert an Speicherposition in 'temp' speichern
+    *a = *b;    // Inhalte tauschen
+    *b = temp;  // Wert aus 'temp' in 'b' speichern
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    int a = 5, b = 10;
+
+    printf("Task 1: Tauschen\n\nVor tauschen\ta=%d\tb=%d\n\n",a,b);
+
+    task1(&a, &b);
+
+    printf("Nach tauschen\ta=%d\tb=%d\n\n\n",a,b);
+
     return 0;
 }
