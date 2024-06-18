686efcf64481e77c9cdeba5e6be72df424ed59bd
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-06-18 20:25:06 +0200

    feat(Bank-Software): add structs

--- 23-24/PRG/Bank-Software/main.c
+++ 23-24/PRG/Bank-Software/main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 
+// Datenmodellierung für eine Banksoftware
+
+struct Datum{
+    int tag;
+    int monat;
+    int jahr;
+};
+
+struct Adresse
+{
+    char strasse[30];
+    char hausnummer[10];
+    char postleitzahl[5];
+    char wohnort[20];
+};
+
+struct Person{
+    char name[30];
+    char vorname[30];
+    struct Adresse adresse;
+    struct Datum geburtstag;
+};
+
+struct Konto {
+    struct Person inhaber;
+    char iban[23]; // 22 + 1 für \0
+    struct Datum eroeffnung;
+    double kontostand;
+    double gebuehr;
+    double zins;
+};
+
 int main()
 {
     printf("Hello World!\n");
