d4ac7f459ea3b57bed71043e79ed33036f72a302
FantasyFlip <>
2024-09-26 18:27:22 +0200

    feat(Modulo7): add project

---
+++ 24-25/PRG/Modulo7/src/Modulo7.java
@@ -0,0 +1,11 @@
+public class Modulo7 {
+    public static void main(String[] args) {
+        int maxCount = 100;
+
+        for(int i = 0; i < maxCount; i++) {
+            if(i % 7 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
