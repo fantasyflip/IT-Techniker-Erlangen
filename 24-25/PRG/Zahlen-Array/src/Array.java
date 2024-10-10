265e4f4766f68e7a710c75d71dfafaf12b40eb35
FantasyFlip <>
2024-10-10 18:14:04 +0200

    feat(Zahlen-Array): custom sort

--- 24-25/PRG/Zahlen-Array/src/Array.java
+++ 24-25/PRG/Zahlen-Array/src/Array.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Array {
     public static void main(String[] args) {
         int numbers [] = new int[5];
@@ -27,7 +25,16 @@ public class Array {
 
         System.out.println("Ausgabe mit Sortierung");
 
-        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    // Swap the elements
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
 
         for(int i = 0; i < numbers.length; i++){
             System.out.println(numbers[i]);
