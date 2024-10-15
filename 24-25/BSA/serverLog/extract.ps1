948b2e9fb9d92ce2646e504fe2fd995e9f072c23
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-10-15 20:23:23 +0200

    feat(serverLog): add solution

---
+++ 24-25/BSA/serverLog/extract.ps1
@@ -0,0 +1,28 @@
+# Read .\server.log line by line
+$serverLog = Get-Content -Path .\server.log
+
+# set the filter
+$filter = "warning"
+
+# create an array to store the collected items
+$collectedItems = @()
+
+foreach($line in $serverLog){
+    # Check if the line contains the filter
+    if($line -match $filter){
+
+        # add to the collected items
+        $collectedItems += $line
+        
+        # get the date using "\d{4}-\d{2}-\d{2}"
+        $date = [regex]::Match($line, "\d{4}-\d{2}-\d{2}").Value
+
+        # get everything after the last ']'
+        $message = $line -replace ".*\] "
+
+        Write-Host "Datum: "$date", Meldung: "$message
+    }
+}
+
+# Export the errors to a log file
+$collectedItems | Out-File -FilePath .\$filter.log
