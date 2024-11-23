6694d2e9edd84bdd95e019e79f94b3984d83ba00
FantasyFlip <>
2024-11-23 10:57:56 +0100

    feat(Videoband): switch to Class Array

--- 24-25/PRG/Videoband/src/VideoVerleih.java
+++ 24-25/PRG/Videoband/src/VideoVerleih.java
@@ -2,18 +2,15 @@ public class VideoVerleih
 {
     public static void main ( String args[] )
     {
-        Videoband artikelA = new Videoband("Jaws", 120 );
-        Videoband artikelB = new Videoband("Star Wars" );
-        Spielfilm artikelSfA = new Spielfilm("Inception",212, "Christopher Nolan", "PG");
-        Musikvideo artikelMvA = new Musikvideo("Rap God", 3, "Eminem", "Rap");
-        Reportage artikelRA = new Reportage("Putins Bären", 101, "20.10.2023");
-        TechnikReportage artikelTRA = new TechnikReportage("Der Fall Boeing", 70, "10.06.2014", "Flugzeuge");
+        TechnikReportage tReportagen [] = new TechnikReportage[5];
+        tReportagen[0] = new TechnikReportage("Der Fall Boeing", 70, "10.06.2014", "Flugzeuge");
+        tReportagen[1] = new TechnikReportage("Der Tesla-Mythos", 85, "22.08.2017", "Elektroautos");
+        tReportagen[2] = new TechnikReportage("Das Geheimnis der Quantencomputer", 95, "15.11.2020", "Computer");
+        tReportagen[3] = new TechnikReportage("Die Wiedergeburt der Raumfahrt", 78, "04.07.2019", "Raumfahrt");
+        tReportagen[4] = new TechnikReportage("Künstliche Intelligenz: Fluch oder Segen?", 90, "12.03.2022", "KI-Technologie");
 
-        artikelA.anzeigen();
-        artikelB.anzeigen();
-        artikelSfA.anzeigen();
-        artikelMvA.anzeigen();
-        artikelRA.anzeigen();
-        artikelTRA.anzeigen();
+        for (TechnikReportage tRep : tReportagen) {
+            tRep.anzeigen();
+        }
     }
 }
