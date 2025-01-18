94810a57001730fba3a72ff7597f0c0ca60096ff
FantasyFlip <>
2025-01-18 09:37:42 +0100

    feat(7-8_Tennisspieler): add solution for task c

--- 24-25/PRG/7-8_Tennisspieler/src/Tennisspieler.java
+++ 24-25/PRG/7-8_Tennisspieler/src/Tennisspieler.java
@@ -3,6 +3,13 @@ public class Tennisspieler {
     public int alter;
 
     public int alterDifferenz(int alter){
+        // c)
         return Math.abs(alter - this.alter);
+
+        // Im Scope der Methode alterDifferenz wird eine lokale Variable erstellt,
+        // die den gleichen Namen wie die Instanzvariable des Objekts trägt.
+        // Um zwischen der Instanzvariable und der lokalen Variable zu unterscheiden
+        // wird die Instanzvariable mit this. angesprochen. "this" referenziert somit
+        // auf das aktuelle Objekt in dem man sich befindet.
     }
 }
