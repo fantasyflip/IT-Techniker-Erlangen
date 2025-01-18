11c047050cba65526c6524646863139cdea83e3a
FantasyFlip <>
2025-01-18 10:16:14 +0100

    feat(7-8_Tennisspieler): add solution for task j

--- 24-25/PRG/7-8_Tennisspieler/src/TennisspielerTest.java
+++ 24-25/PRG/7-8_Tennisspieler/src/TennisspielerTest.java
@@ -13,5 +13,14 @@ public class TennisspielerTest {
         // Der Zugriff auf die Variablen und Methoden des Objekt erfolgt mittels der Punkt-Notation.
         // Das Alter wird zum Beispiel über maier.alter angesprochen.
         // Die Methode wird über maier.alterDifferenz(5) aufgerufen.
+
+        //j)
+        Tennisspieler schmid = new Tennisspieler("G. Schmid", 45, maier);
+        Tennisspieler berger = new Tennisspieler("I. Berger", 36, schmid);
+
+        // Beim erstellen von maier wird die startNummer von maier auf 1 gesetzt und die folgeNummer in der gesammten Klasse Tennisspieler auf 2 erhöht.
+        // Beim erstellen von schmid wird die startNummer von schmid auf 2 gesetzt und die folgeNummer in der gesamten Klasse Tennisspieler auf 3 erhöht.
+        // Beim erstellen von berger wird die startNummer von berger auf 3 gesetzt und die folgeNummer in der gesamten Klasse Tennisspieler auf 4 erhöht.
+
     }
 }
