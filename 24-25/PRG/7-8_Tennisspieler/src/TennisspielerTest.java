6896dba2348dc3f5a91b4bf60d38b4a0001fd376
FantasyFlip <>
2025-01-18 09:34:30 +0100

    feat(7-8_Tennisspieler): add solution for task b

---
+++ 24-25/PRG/7-8_Tennisspieler/src/TennisspielerTest.java
@@ -0,0 +1,12 @@
+public class TennisspielerTest {
+    public static void main(String[] args) {
+
+        // b)
+        Tennisspieler maier;
+        maier = new Tennisspieler();
+
+        // Durch die erste Zeile wird die Variable maier vom Typ Tennisspieler definiert.
+        // Zu diesem Zeitpunkt ist sie ein Pointer auf null.
+        // Erst durch die Initialisierung in der zweiten Zeile wird ein Objekt vom Typ Tennisspieler erstellt.
+    }
+}
