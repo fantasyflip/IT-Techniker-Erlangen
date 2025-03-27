34057f745c6b0447fa4fecc2688c5e2ea1ef198b
FantasyFlip <>
2025-03-27 18:46:34 +0100

    chore(11-16_Fertiggerichte): add comments

--- 24-25/PRG/11-16_Fertiggerichte/src/Freezer.java
+++ 24-25/PRG/11-16_Fertiggerichte/src/Freezer.java
@@ -6,6 +6,7 @@ public class Freezer {
     Map<ReadyMeal, Integer> items = new HashMap<ReadyMeal, Integer>();
 
     public Freezer() {
+        // Add default items
         items.put(new ReadyMeal("Pizza", 5.99),2);
         items.put(new ReadyMeal("Nudeln", 3.99),5);
         items.put(new ReadyMeal("Pommes", 4.99),1);
@@ -13,6 +14,7 @@ public class Freezer {
     }
 
     private  void printSectionSeparator(){
+        // Print a separator line
         for(int i = 0; i < 3; i++){
             System.out.println();
         }
@@ -25,6 +27,7 @@ public class Freezer {
     }
 
     private int getTaskNumber(){
+        // Function to display the list of possible tasks a user can pick from
         System.out.println("Was möchten Sie tun?");
         System.out.println();
         System.out.println("1. Inhalt anzeigen");
@@ -32,7 +35,7 @@ public class Freezer {
         System.out.println("3. Verzehr verbuchen");
         System.out.println("4. Beenden");
 
-        // User input
+        // Read user input
         Scanner scanner = new Scanner(System.in);
         System.out.print("Auswahl: ");
         int taskNumber = scanner.nextInt();
@@ -43,6 +46,8 @@ public class Freezer {
     }
 
     public void selectAction(){
+        // Entry point into all actions for the user
+        // Runs until stopped by case 4
         boolean runLoop = true;
 
         while(runLoop) {
@@ -75,13 +80,14 @@ public class Freezer {
     }
 
     public void printItems(boolean displayIndex){
-
+        // Function to print all available Items
         System.out.println("Schrankinhalt");
         System.out.println();
 
         int i = 0;
 
         for (Map.Entry<ReadyMeal, Integer> entry : items.entrySet()) {
+            // If displayIndex is true, a index will be displayed in front of each item
             if(displayIndex){
                 System.out.print(i + ": ");
                 i++;
@@ -93,9 +99,10 @@ public class Freezer {
     }
 
     public void addItem(){
+        // Function to add an item to the map
         System.out.println();
 
-
+        // Reading user inputs
         System.out.print("Name des Fertiggerichts: ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
@@ -104,13 +111,15 @@ public class Freezer {
         System.out.print("Preis des Fertiggerichts: ");
         Double price = scanner.nextDouble();
 
+        // Creating new ReadyMeal
         ReadyMeal f = new ReadyMeal(name, price);
 
-        //check if same item is already in map
+        // check if same item is already in map
         if(items.containsKey(f)){
-            //if it is already in the map increase count by one
+            // if it is already in the map increase count by one
             items.put(f, items.get(f) + 1);
         } else {
+            // otherwise add item
             items.put(f, 1);
         }
 
@@ -120,6 +129,9 @@ public class Freezer {
     }
 
     public void removeItem() {
+        // Function to remove an item from the map
+
+        // Return if the freezer has no items
         if (items.isEmpty()) {
             System.out.println("Der Schrank ist leer. Es gibt nichts zu entfernen.");
             printSectionSeparator();
@@ -128,6 +140,7 @@ public class Freezer {
 
         printItems(true);
 
+        // Reading user input
         System.out.println("Welches Fertiggericht soll entfernt werden? Nummer:");
         Scanner scanner = new Scanner(System.in);
 
@@ -136,6 +149,7 @@ public class Freezer {
             itemIndex = scanner.nextInt();
         } catch (Exception e) {
             System.out.println("Ungültige Eingabe. Bitte eine Zahl eingeben.");
+            removeItem();
             return;
         }
 
