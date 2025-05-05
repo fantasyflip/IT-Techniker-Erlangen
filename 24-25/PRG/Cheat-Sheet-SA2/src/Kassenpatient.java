f85b18480638d91b8cea83af5860f7a8b8558118
Tuettensuppe <161838331+Tuettensuppe@users.noreply.github.com>
2025-05-05 17:35:53 +0200

    Update Kassenpatient.java
    Added Exception

--- 24-25/PRG/Cheat-Sheet-SA2/src/Kassenpatient.java
+++ 24-25/PRG/Cheat-Sheet-SA2/src/Kassenpatient.java
@@ -1,9 +1,36 @@
 public class Kassenpatient extends Patient {
     private boolean familienversichert;
 
-    public Kassenpatient(String name, String vorname, int alter, boolean familienversichert) {
-        // Mit "super(...)" kann der Konstruktor von der Elternklasse aufgerufen werden.
+    /**
+     * Konstruktor für die Klasse Kassenpatient.
+     * Initialisiert einen neuen Kassenpatienten.
+     *
+     * WICHTIG: Dieser Konstruktor muss "throws PraxisVollException" deklarieren,
+     * weil der Aufruf des Elternkonstruktors mit "super(name, vorname, alter)"
+     * eine PraxisVollException werfen kann.
+     *
+     * Da PraxisVollException eine "checked exception" ist (vom Compiler geprüft),
+     * muss jede Methode (einschließlich Konstruktoren), die potenziell eine solche
+     * Ausnahme werfen kann (sei es direkt oder durch Aufruf einer anderen Methode),
+     * diese Ausnahme in ihrer Signatur mit "throws" deklarieren.
+     *
+     * Der Kassenpatient-Konstruktor wirft die Ausnahme nicht selbst direkt,
+     * sondern er muss deklarieren, dass die Ausnahme, die vom "super()"-Aufruf
+     * kommen kann, an den Aufrufer des Kassenpatient-Konstruktors weitergegeben wird.
+     *
+     * @param name Der Name des Patienten.
+     * @param vorname Der Vorname des Patienten.
+     * @param alter Das Alter des Patienten.
+     * @param familienversichert Gibt an, ob der Patient familienversichert ist.
+     * @throws PraxisVollException Wird vom Elternkonstruktor geworfen und hier weitergegeben,
+     *                              wenn die Praxis voll ist.
+     */
+    public Kassenpatient(String name, String vorname, int alter, boolean familienversichert) throws PraxisVollException {
+        // Mit "super(...)" wird der Konstruktor der Elternklasse aufgerufen.
+        // Dieser Aufruf kann eine PraxisVollException werfen, da der Elternkonstruktor dies deklariert.
+        // Deshalb muss auch dieser Konstruktor die Ausnahme deklarieren.
         super(name, vorname, alter);
+
         this.familienversichert = familienversichert;
     }
 
